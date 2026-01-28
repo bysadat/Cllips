c2d06598b5db420c5b4b57e61badd00087e596b0
Bashiru Yerima Sadat <124477655+bysadat@users.noreply.github.com>
2026-01-28 02:03:30 -0500

    Wroked on the Hero section and the snippet section

--- Clips/tailwind.config.js
+++ Clips/tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        PrimaryGold: 'hsl(43, 96%, 56%)',
+        DeepIndigo: 'hsl(245, 50%, 60%)',
+        RichCharcoal: 'hsl(220, 20%, 20%)',
+        SoftSlate: 'hsl(215, 15%, 55%)',
+      },
+      fontFamily: {
+        sans: ['Quicksand', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
